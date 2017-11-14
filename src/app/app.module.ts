c20de1c8207809b36dda8bb11bd4761198569239
Phùng Đức Nhật <ducnhatphung@gmail.com>
2017-11-14 11:11:43 +0700

    feat: search-input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,9 @@ import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform
 import { AppComponent } from './app.component';
 import { HomePageRightComponent } from './home-page/home-page-right/home-page-right.component';
 import { HomePageLeftComponent } from './home-page/home-page-left/home-page-left.component';
-import { SearchResultPageLeftComponent } from './home-page/search-result-page-left/search-result-page-left.component';
-import { SearchResultPageRightComponent } from './home-page/search-result-page-right/search-result-page-right.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { SearchPageLeftComponent } from './search-page-left/search-page-left.component';
-import { SearchPageRightComponent } from './search-page-right/search-page-right.component';
+import { SearchPageLeftComponent } from './search-page/search-page-left/search-page-left.component';
+import { SearchPageRightComponent } from './search-page/search-page-right/search-page-right.component';
 
 const appRoutes: Routes = [
   {
@@ -26,8 +24,7 @@ const appRoutes: Routes = [
     component: HomePageComponent,
     children: [
       { path: '', component: SearchPageLeftComponent, outlet: 'left-side' },
-      { path: '', component: SearchPageRightComponent, outlet: 'right-side' },
-      { path: 'query', component: SearchResultPageRightComponent, outlet: 'right-side' }
+      { path: '', component: SearchPageRightComponent, outlet: 'right-side' }
     ]
   }
 ];
@@ -37,11 +34,9 @@ const appRoutes: Routes = [
     AppComponent,
     HomePageRightComponent,
     HomePageLeftComponent,
-    SearchResultPageLeftComponent,
-    SearchResultPageRightComponent,
     HomePageComponent,
     SearchPageLeftComponent,
-    SearchPageRightComponent
+    SearchPageRightComponent,
   ],
   imports: [
     BrowserModule,
